f2b95518d6f72cf81e007a7aad254c2816182edf
Eugen Ganscha <eganscha@gmail.com>
2025-05-27 14:25:44 +0200

    TagCards now behave properly like links

--- services/frontend/sakura-notes-frontend/src/components/TagCard/TagCard.jsx
+++ services/frontend/sakura-notes-frontend/src/components/TagCard/TagCard.jsx
@@ -3,26 +3,30 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import {CardActionArea} from "@mui/material";
+import {Link} from "react-router";
+import styles from "../LogoWithText/LogoWithText.module.css";
 
 function TagCard( {tag} ) {
     return (
-        <Card sx={{ minWidth: 230, height: 90,
-            display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-        }}>
-            <CardActionArea onClick={() => console.log('Card clicked')} sx={{
-                transition: 'background-color 0.2s ease-in',
-                width: '100%', height: '100%',
-                '&:hover': {
-                    backgroundColor: 'var(--bg-medium)'
-                }
+        <Link to={`/notes?tag=${tag}`} className={Styles.link}>
+            <Card sx={{ minWidth: 230, height: 90,
+                display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
             }}>
-                <CardContent>
-                    <Typography variant="h5" component="div">
-                        { tag }
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+                <CardActionArea sx={{
+                    transition: 'background-color 0.2s ease-in',
+                    width: '100%', height: '100%',
+                    '&:hover': {
+                        backgroundColor: 'var(--bg-medium)'
+                    }
+                }}>
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            { tag }
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Link>
     );
 }
 

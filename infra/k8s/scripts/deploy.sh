9cc4044945df6bbab2f5a3edd8b8dacfef1a3991
Eugen Ganscha <eganscha@gmail.com>
2025-06-30 14:49:16 +0200

    Backend now has a volume associated with it. Renamed volume of db to match docker-compose naming conventions.

--- infra/k8s/scripts/deploy.sh
+++ infra/k8s/scripts/deploy.sh
@@ -5,10 +5,10 @@ kubectl apply -f ../common/namespace.yaml
 kubectl apply -f ../common/global-config.yaml
 kubectl apply -f ../common/global-secrets.yaml
 
-# Backend
-kubectl apply -f ../backend/deployment.yaml
-kubectl apply -f ../backend/service.yaml
-
 # DB
 kubectl apply -f ../db/statefulset.yaml
 kubectl apply -f ../db/service.yaml
+
+# Backend
+kubectl apply -f ../backend/deployment.yaml
+kubectl apply -f ../backend/service.yaml

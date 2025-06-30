1894b304da548d850e6835a0a9b3a3ba5f8ef4ef
Eugen Ganscha <eganscha@gmail.com>
2025-06-30 13:48:27 +0200

    Basic DB k8s setup

--- infra/k8s/scripts/build.sh
+++ infra/k8s/scripts/build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 BACKEND_IMG=sakura-notes-backend
+DB_IMG=sakura-notes-redis-db
 
 docker build -t $BACKEND_IMG:latest ../../../services/backend/sakura-notes-laravel-api
+docker build -t $DB_IMG:latest ../../../services/db
 
 mkdir -p tmp
 docker save -o tmp/$BACKEND_IMG.tar $BACKEND_IMG:latest
+docker save -o tmp/$DB_IMG.tar $DB_IMG:latest
 
 minikube image load tmp/$BACKEND_IMG.tar
+minikube image load tmp/$DB_IMG.tar
 
 rm -r ./tmp
--- infra/k8s/scripts/deploy.sh
+++ infra/k8s/scripts/deploy.sh
@@ -8,3 +8,7 @@ kubectl apply -f ../common/global-secrets.yaml
 # Backend
 kubectl apply -f ../backend/deployment.yaml
 kubectl apply -f ../backend/service.yaml
+
+# DB
+kubectl apply -f ../db/statefulset.yaml
+kubectl apply -f ../db/service.yaml

5859822cf64fa76695d0243c20a774376ce71d7a
Eugen Ganscha <eganscha@gmail.com>
2025-07-08 20:16:28 +0200

    Adjustments to start.sh

--- infra/k8s/scripts/start.sh
+++ infra/k8s/scripts/start.sh
@@ -1,38 +1,9 @@
 #!/bin/bash
 
-# At least one parameter has to be passed to the script to determine the target O.S.
-# The script accepts no more than two parameters
-if [[ $# -lt 1 ]] || [[ $# -gt 2 ]]; then
-    echo "Invalid number of parameters passed. Expected: 1-2, received: $#"
-    echo "\$0 (required) - Target O.S. (linux, macOS)"
-    echo "\$1 (optional) - Target Mode (ingress, nginx)"
-
-    exit 1
-fi
-
-# Make sure the first argument passed is a valid target O.S.
-# (no default can be given here, as we don't know for certain the O.S. the user is running)
-TARGET_OS=""
-case "$1" in
-    linux|Linux|LINUX)
-        echo "TARGET_OS=\"linux\""
-        TARGET_OS="linux"
-        ;;
-    macos|macOS|MACOS)
-        echo "TARGET_OS=\"macos\""
-        TARGET_OS="macos"
-        ;;
-    *)
-        echo "Param \$1 was invalid. Expected Target O.S. in the form of (linux, macOS)"
-        exit 1
-        ;;
-esac
-
-# Default mode is ingress, if no second parameter is passed
+# Default mode is ingress, if no parameter is passed
 MODE="ingress"
-# Change the default mode, if a valid mode argument has been passed as $2
-if [[ $# -gt 1 ]]; then
-    case "$2" in
+if [[ $# -gt 0 ]]; then
+    case "$1" in
         nginx|Nginx|NGINX)
             echo "Starting sakura-notes application in nginx mode."
             MODE="nginx"
@@ -56,19 +27,6 @@ if ! minikube status >/dev/null 2>&1; then
 else
     echo "Minikube is already running. Please make sure the required addons for your target mode are enabled."
 fi
-
-# Run minikube tunnel as a background process if the mode is ingress, the target O.S. is macos
-# and minikube tunnel is not already running
-# if [[ "$MODE" == "ingress" ]] && [[ "$TARGET_OS" == "macos" ]]; then
-#     if ! ps aux | grep '[m]inikube tunnel' >/dev/null 2>&1; then
-#         echo "starting minikube tunnel via sudo for macOS in the background."
-#         # (-p fails silently, if the script is re-run)
-#         mkdir -p tmp
-#         sudo nohup minikube tunnel > "./tmp/minikube-tunnel-pid.log" 2>&1 &
-#     else
-#         echo "minikube tunnel already running."
-#     fi
-# fi
 
 # Build & Deploy
 ./build.sh "$MODE"
--- services/frontend/sakura-notes-frontend/vite.config.js
+++ services/frontend/sakura-notes-frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,                       // listen on 0.0.0.0 inside the container
-    port: 5173,                       // (optional, keeps the default)
+    port: 5173,
     allowedHosts: ['frontend', 'sakura.notes']
   }
 })

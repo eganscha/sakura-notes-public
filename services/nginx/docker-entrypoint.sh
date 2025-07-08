c3f3c5a66c549d5d7515eddb6df21ef778b995b4
Eugen Ganscha <eganscha@gmail.com>
2025-07-08 23:42:34 +0200

    nginx.conf file is now created more dynamically, this allows both k8s and docker-compose to use the same file with different backend services.

--- services/nginx/docker-entrypoint.sh
+++ services/nginx/docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Substitute the env variable inside of the nginx.conf.template and pass the output into an nginx.conf file
+envsubst '${NGINX_BACKEND_SERVERS}' \
+    < /etc/nginx/nginx.conf.template \
+    > /etc/nginx/nginx.conf
+
 # Execute the CMD command as a command,
 # don't treat it as a parameter
 exec "$@"

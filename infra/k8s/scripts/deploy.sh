ab0d16abbb9e6ac5a9cea7a8888de0f0de4c60a0
Eugen Ganscha <eganscha@gmail.com>
2025-07-10 01:46:38 +0200

    Adjusted project structure to always use Ingress when running via k8s, as it's the only sensible default.

--- infra/k8s/scripts/deploy.sh
+++ infra/k8s/scripts/deploy.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Default mode is ingress, if no parameters are passed
-MODE="ingress"
-# Change the default mode, if a valid mode argument has been passed
-if [[ $# -gt 0 ]]; then
-    case "$1" in
-        nginx|Nginx|NGINX)
-            MODE="nginx"
-            ;;
-        *)
-            MODE="ingress"
-            ;;
-    esac
-fi
-
 # Global Files First
 kubectl apply -f ../common/namespace.yaml
 kubectl apply -f ../common/global-config.yaml
@@ -33,26 +19,17 @@ kubectl apply -f ../backend/service.yaml
 kubectl apply -f ../frontend/deployment.yaml
 kubectl apply -f ../frontend/service.yaml
 
-# Apply the .pem certs, if Ingress it the target mode and tls-secrets don't exist yet
-if [[ "$MODE" == "ingress" ]]; then
-    if ! kubectl get secret sakura-notes-tls -n sakura-notes >/dev/null 2>&1; then
-        echo "Creating TLS secret \"sakura-notes-tls\"."
+# Ingress
+# Apply the .pem certs if the Ingress tls-secrets don't exist yet
+if ! kubectl get secret sakura-notes-tls -n sakura-notes >/dev/null 2>&1; then
+    echo "Creating TLS secret \"sakura-notes-tls\"."
 
-        kubectl create secret tls sakura-notes-tls \
-        --namespace sakura-notes \
-        --cert=../../../services/ingress/ssl_local_certificate/fullchain.pem \
-        --key=../../../services/ingress/ssl_local_certificate/privkey.pem
-    else
-        echo "TLS secret \"sakura-notes-tls\" already present."
-    fi
-fi
-
-# Start with either Nginx, or Ingress, depending on the passed mode (or default to Ingress)
-if [[ "$MODE" == "nginx" ]]; then
-    # Nginx
-    kubectl apply -f ../nginx/deployment.yaml
-    kubectl apply -f ../nginx/service.yaml
+    kubectl create secret tls sakura-notes-tls \
+    --namespace sakura-notes \
+    --cert=../../../services/ingress/ssl_local_certificate/fullchain.pem \
+    --key=../../../services/ingress/ssl_local_certificate/privkey.pem
 else
-    # Ingress
-    kubectl apply -f ../ingress/ingress.yaml
+    echo "TLS secret \"sakura-notes-tls\" already present."
 fi
+
+kubectl apply -f ../ingress/ingress.yaml
--- infra/k8s/scripts/start.sh
+++ infra/k8s/scripts/start.sh
@@ -1,44 +1,13 @@
 #!/bin/bash
 
-# Default mode is ingress, if no parameter is passed
-MODE="ingress"
-if [[ $# -gt 0 ]]; then
-    case "$1" in
-        nginx|Nginx|NGINX)
-            echo "Starting sakura-notes application in nginx mode."
-            MODE="nginx"
-            ;;
-        *)
-            echo "Starting sakura-notes application in ingress mode."
-            MODE="ingress"
-            ;;
-    esac
-fi
-
 # Start minikube with the required addons, if it's not already running
 if ! minikube status >/dev/null 2>&1; then
-    if [[ "$MODE" == nginx ]]; then
-        echo "Starting Minikube with ingress DISABLED."
-        minikube start --addons=storage-provisioner,default-storageclass,registry
-    else
-        echo "Starting Minikube with ingress ENABLED."
-        minikube start --addons=storage-provisioner,default-storageclass,ingress,registry
-    fi
+    echo "Starting Minikube with ingress ENABLED."
+    minikube start --addons=storage-provisioner,default-storageclass,ingress,registry
 else
     echo "Minikube is already running. Please make sure the required addons for your target mode are enabled."
 fi
 
 # Build & Deploy
-./build.sh "$MODE"
-./deploy.sh "$MODE"
-
-# Nginx Port Forward, if nginx has been selected as the mode
-if [[ "$MODE" == "nginx" ]]; then
-    # Wait for nginx pod to become ready and then forward both http and https traffic
-    echo "Waiting for nginx pod to become ready."
-    kubectl wait --for=condition=ready pod -l app=nginx -n sakura-notes --timeout=60s
-
-    echo "Nginx port-forward enabled for 8000:80 and 8001:443."
-    kubectl port-forward services/nginx 8000:80 -n sakura-notes &
-    kubectl port-forward services/nginx 8001:443 -n sakura-notes
-fi
+./build.sh
+./deploy.sh
